4c56eb5070cf14910d779dffd0d394e0b09c5eba
jiponcevela-png <jiponcevela@gmail.com>
2025-11-24 11:26:03 -0500

    Delete PruebaProgreso Velasco directory

--- PruebaProgreso Velasco/ventas.h
+++
@@ -1,9 +0,0 @@
-#ifndef VENTAS_H
-#define VENTAS_H
-#define MAX_DIAS 30
-void ingresarVentas(float ventas[], int n);
-float totalVentas(float ventas[], int n);
-float promedioVentas(float ventas[], int n);
-float ventaMaxima(float ventas[], int n);
-void mostrarResultados(float total, float promedio, float maxima);
-#endif
